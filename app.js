7a9090b15e43a8883498efa1873142b91a04c721
cc <308694702@qq.com>
2020-03-18 21:33:12 +0800

    c

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const response = require('./middlewares/response')
 const bodyParser = require('koa-bodyparser')
 const config = require('./config')
 var cors=require('koa2-cors');
+
+
+const router = require('koa-router')({
+    prefix: '/heyushuo'
+})
+router.get('/index/index',function(ctx,body){
+    console.log('i am');
+    console.log('ctx',ctx)
+    ctx.body='i am ready'
+})
+
 app.use(cors())
 
 // 使用响应处理中间件
@@ -14,8 +25,9 @@ app.use(response)
 app.use(bodyParser())
 
 // 引入路由分发
-const router = require('./routes')
-app.use(router.routes()) // 添加路由中间件
+//const router = require('./routes')
+//app.use(router.routes()) // 添加路由中间件
+app.use(router.routes())
 
 // 启动程序，监听端口
 app.listen(config.port, () => debug(`listening on port ${config.port}`))
